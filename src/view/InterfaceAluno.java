f414aca0c68f3193aa6620deea3cfd794bffd4fb
brunohsp <bruno.henriquepereira1707@gmail.com>
2024-04-12 11:49:26 -0300

    feat: add input verification to numbers and names

--- src/view/InterfaceAluno.java
+++ src/view/InterfaceAluno.java
@@ -2,6 +2,8 @@ package view;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import dao.AlunoDao;
 import dao.NotaDao;
@@ -11,6 +13,31 @@ import models.Estatistica;
 public class InterfaceAluno {
 
   private static final Scanner scanner = new Scanner(System.in);
+
+  private static Aluno findAluno(ArrayList<Aluno> alunos) {
+    while (true) {
+      while (!scanner.hasNextInt()) {
+        System.out.println("Entrada inválida! Por favor, digite um número inteiro.");
+        scanner.next();
+      }
+
+      int selecao = scanner.nextInt();
+
+      for (Aluno aluno : alunos) {
+        if (aluno.getIdAluno().equals(selecao)) {
+          return aluno;
+        }
+      }
+      System.out.println("Entrada inválida! Por favor, digite um id válido.");
+    }
+  }
+
+  private static boolean isValidName(String nome) {
+    String regex = "^[a-zA-ZÀ-ÿ]+(?:\\s[a-zA-ZÀ-ÿ]+)*$";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(nome);
+    return matcher.matches();
+  }
 
   private static void clearScreen() {
     System.out.print("\033[H\033[2J");
@@ -27,11 +54,12 @@ public class InterfaceAluno {
       Estatistica estatistica = NotaDao.getEstatistica(aluno);
 
       System.out.print(aluno.getIdAluno() + " | " + aluno.getNome() + " | ");
-      if (estatistica != null)
+      if (estatistica != null) {
         System.out.print(estatistica.getMedia() + " | " + estatistica.getMin() + " | " + estatistica.getMax() + " | "
             + estatistica.getVar() + "\n");
-      else
+      } else {
         System.out.println("");
+      }
     });
     scanner.nextLine();
   }
@@ -39,8 +67,12 @@ public class InterfaceAluno {
   public static void create() {
     clearScreen();
     System.out.println("Digite o nome do novo aluno:");
-
     String nome = scanner.nextLine();
+
+    while (!isValidName(nome)) {
+      System.out.println("Nome inválido! Por favor, insira um nome válido.");
+      nome = scanner.nextLine();
+    }
 
     Aluno aluno = new Aluno();
     aluno.setNome(nome);
@@ -56,26 +88,23 @@ public class InterfaceAluno {
     System.out.println("Digite número do aluno para alterá-lo:");
     ArrayList<Aluno> alunos = AlunoDao.get();
 
-    int contador = 0;
     for (Aluno aluno : alunos) {
-      contador += 1;
-      System.out.println(contador + " | " + aluno.getNome());
+      System.out.println(aluno.getIdAluno() + " | " + aluno.getNome());
     }
 
-    int selecao = scanner.nextInt();
+    Aluno alunoModificado = InterfaceAluno.findAluno(alunos);
 
-    while (selecao < 1 || selecao > alunos.size()) {
-      System.out.println("Entrada inválida");
-      selecao = scanner.nextInt();
-    }
-
-    Aluno novoAluno = alunos.get(selecao - 1);
     System.out.println("Digite o novo nome:");
     scanner.nextLine();
     String nome = scanner.nextLine();
 
-    novoAluno.setNome(nome);
-    AlunoDao.update(novoAluno);
+    while (!isValidName(nome)) {
+      System.out.println("Nome inválido! Por favor, insira um nome válido.");
+      nome = scanner.nextLine();
+    }
+
+    alunoModificado.setNome(nome);
+    AlunoDao.update(alunoModificado);
 
     System.out.println("Aluno alterado com sucesso!");
     System.out.println("PRESSIONE ENTER PARA SAIR");
@@ -87,20 +116,13 @@ public class InterfaceAluno {
     System.out.println("Digite número do aluno para deletá-lo:");
     ArrayList<Aluno> alunos = AlunoDao.get();
 
-    int contador = 0;
     for (Aluno aluno : alunos) {
-      contador += 1;
-      System.out.println(contador + " | " + aluno.getNome());
+      System.out.println(aluno.getIdAluno() + " | " + aluno.getNome());
     }
 
-    int selecao = scanner.nextInt();
+    Aluno alunoDelete = InterfaceAluno.findAluno(alunos);
 
-    while (selecao < 1 || selecao > alunos.size()) {
-      System.out.println("Entrada inválida");
-      selecao = scanner.nextInt();
-    }
-
-    AlunoDao.delete(alunos.get(selecao - 1));
+    AlunoDao.delete(alunoDelete);
     System.out.println("Aluno removido com sucesso!");
     System.out.println("PRESSIONE ENTER PARA SAIR");
     scanner.nextLine();
